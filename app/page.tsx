8251c2529f5929a5a732a6e50a50a96aecaba414
thematthewdonkor <thematthewdonkor@gmail.com>
2025-09-13 17:39:34 +0000

    title color change

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { ResponseChat } from "@/components/response-chat";
 export default async function HomePage() {
   return (
     <div className="font-sans flex flex-col items-center justify-between min-h-screen p-8 sm:p-8 bg-gray-50">
-      <h1 className="text-2xl sm:text-3xl font-bold text-gray-800 text-center mb-4">
-        QuickChat AI
+      <h1 className="text-2xl sm:text-3xl font-bold text-gray-800 text-balance text-center mb-4">
+        How Can I <span className="text-sky-700">Assist You Today?</span>
       </h1>
 
       <div className="flex-1 w-full max-w-3xl flex flex-col justify-between gap-6">
